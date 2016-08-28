7d2c77d1021a84b89c377d44eac2d5e758f9561c
jessica kilby <jessicakilby@gmail.com>
2016-08-28 16:31:04 -0500

    array-methods

---
+++ README.md
@@ -0,0 +1,20 @@
+#Javascript Arrays: Array-Methods
+
+## Description
+Practice with array methods: 
+* join
+* slice
+* forEach 
+* map 
+* filter
+* reduce
+
+## How to run
+In your terminal run:
+```
+http-server -p 9999
+```
+then navigate to [here](http://localhost:9999) in your browser.
+
+## Contributors
+- [Jessica Kilby](https://github.com/jessicakilby)
---
+++ array-methods.js
@@ -0,0 +1,54 @@
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+console.log("Original planets array: ", planets);
+/*
+ Use the forEach method to add the name of each planet
+ to a div element in your HTML with an id of "planets".
+*/
+
+function print(message){
+  var el = document.getElementById('planets');
+  el.innerHTML = message;
+}
+planets.forEach(print);
+html = "<div>" + planets.join(", ") + "</div>";
+print(html);
+console.log("Print to div array: ", html);
+
+
+// Use the map method to create a new array where the first letter of each planet is capitalized
+function cap(word){
+	for (var i = 0; i < word.length; i++) {
+		var firstLetter = word[0];
+		var firstLetterUpper = firstLetter.toUpperCase();
+		var minusFirst = word.slice(1, word.length);
+		return firstLetterUpper + minusFirst;
+	}
+}
+var titleCase = planets.map(cap);
+console.log("Array in title case: ", titleCase);
+
+
+// Use the filter method to create a new array that contains planets with the letter 'e'
+
+function filterIt(word){
+	for (var i = 0; i < word.length; i++) {
+		if(word[i] === 'e') {
+			return word;
+		}
+	}	
+}
+var filtPlanets = planets.filter(filterIt);
+console.log("filtered by 'e': ", filtPlanets);
+
+
+// Use the reduce method to create a sentence from the words in the following array
+
+var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+function sentMaker(word){
+	var newWords = words.join(" ");
+	return newWords;
+}
+
+var sentence = words.reduce(sentMaker);
+console.log("reduce to a sentence: ", sentence+".");
